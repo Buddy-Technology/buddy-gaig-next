ae6216a763e4f66d35a250e010cb1653ed071a62
Kevin Power <kevinpower@kpowers-air-m1.lan>
2024-03-11 22:32:50 -0400

    another deploy test with a buddy element

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 'use client';
 
+import BuddyOfferElement, { useConfig } from '@buddy-technology/offer-component';
 import Head from 'next/head';
 import Nav from '@components/Nav';
 import Hero from '@components/Hero';
@@ -9,8 +10,32 @@ import Leagues from '@components/Leagues';
 import FeaturedTeam from '@components/FeaturedTeam';
 import Footer from '@components/Footer';
 
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+function OfferElement() {
+  const { config, isLoading } = useConfig(
+    "https://staging.embed.buddy.insure/gaig/pomi/gaig-pomi-accident-config-react.js"
+  );
+
+  if (isLoading || !config) return <Loading />;
+
+  return (
+    <div className="App w-full">
+      <BuddyOfferElement
+        ion="GAIG_POMI_ACCIDENT"
+        partnerID="p-buddytest"
+        stage="STAGING"
+        theme={config.themeBase}
+        onUserEvent={config.userEvents}
+      />
+    </div>
+  );
+}
 
 export default function Home() {
+
   return (
     <div className="container" style={{ maxWidth: "100% !important" }}>
       <Head>
@@ -22,6 +47,7 @@ export default function Home() {
         <Nav />
         <Hero />
         <RTB />
+        <OfferElement />
         <Testimonials />
         <FeaturedTeam />
       </main>
